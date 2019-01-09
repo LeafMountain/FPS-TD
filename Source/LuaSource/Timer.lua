8918beda422db718fd0b8d05bc83282d5ed79a9b
PappaBjorn <vol1@live.se>
2019-01-09 11:52:01 +0100

    Update Timer.lua

--- Source/LuaSource/Timer.lua
+++ Source/LuaSource/Timer.lua
@@ -1,6 +1,9 @@
-MaxTimer = 60
-CurrentTimer = 60
+MaxTimer = 120
+CurrentTimer = 120
 LevelStart = false
 
-function Timer(DeltaTime)
-	if LevelStart = true &&
+function LevelTimer(DeltaTime)
+	if LevelStart = true and CurrentTimer > 0 and CurrentTimer <= MaxTimer then
+		CurrentTimer = CurrentTimer - DeltaTime
+	end
+end
